c759feb967c3348517e903ac641105f093e456a1
Abi أب <el@elabee.me>
2025-07-01 07:21:27 +0700

    use macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,127 +1,63 @@
 #![cfg_attr(windows, feature(abi_vectorcall))]
+
+use ext_php_rs::convert::IntoZval;
+use ext_php_rs::flags::DataType;
 use ext_php_rs::prelude::*;
+use ext_php_rs::types::Zval;
+use std::path::PathBuf;
 
-/// Returns the path to the user's home directory.
-#[php_function]
-pub fn home_dir() -> Option<String> {
-    dirs::home_dir()?.to_str().map(|path| path.to_string())
+#[derive(Debug)]
+pub struct PhpPath(PathBuf);
+
+impl From<PathBuf> for PhpPath {
+    fn from(path: PathBuf) -> Self {
+        PhpPath(path)
+    }
 }
 
-/// Returns the path to the user's cache directory.
-#[php_function]
-pub fn cache_dir() -> Option<String> {
-    dirs::cache_dir()?.to_str().map(|path| path.to_string())
+impl IntoZval for PhpPath {
+    const TYPE: DataType = DataType::String;
+
+    fn set_zval(self, zv: &mut Zval, persistent: bool) -> ext_php_rs::error::Result<()> {
+        self.0
+            .to_str()
+            .map_or(Err(ext_php_rs::error::Error::ZvalConversion(DataType::String)), |str| {
+                zv.set_string(str, persistent)
+            })
+    }
 }
 
-/// Returns the path to the user's config directory.
-#[php_function]
-pub fn config_dir() -> Option<String> {
-    dirs::config_dir()?.to_str().map(|path| path.to_string())
+/// Macro to generate directory functions
+macro_rules! dirs_fn {
+    ($fn_name:ident, $doc:expr) => {
+        #[doc = $doc]
+        #[php_function]
+        pub fn $fn_name() -> Option<PhpPath> {
+            dirs::$fn_name().map(PhpPath)
+        }
+    };
 }
 
-/// Returns the path to the user's local config directory.
-#[php_function]
-pub fn config_local_dir() -> Option<String> {
-    dirs::config_local_dir()?
-        .to_str()
-        .map(|path| path.to_string())
-}
-
-/// Returns the path to the user's data directory.
-#[php_function]
-pub fn data_dir() -> Option<String> {
-    dirs::data_dir()?.to_str().map(|path| path.to_string())
-}
-
-/// Returns the path to the user's local data directory.
-#[php_function]
-pub fn data_local_dir() -> Option<String> {
-    dirs::data_local_dir()?
-        .to_str()
-        .map(|path| path.to_string())
-}
-
-/// Returns the path to the user's executable directory.
-#[php_function]
-pub fn executable_dir() -> Option<String> {
-    dirs::executable_dir()?
-        .to_str()
-        .map(|path| path.to_string())
-}
-
-/// Returns the path to the user's preference directory.
-#[php_function]
-pub fn preference_dir() -> Option<String> {
-    dirs::preference_dir()?
-        .to_str()
-        .map(|path| path.to_string())
-}
-
-/// Returns the path to the user's runtime directory.
-#[php_function]
-pub fn runtime_dir() -> Option<String> {
-    dirs::runtime_dir()?.to_str().map(|path| path.to_string())
-}
-
-/// Returns the path to the user's state directory.
-#[php_function]
-pub fn state_dir() -> Option<String> {
-    dirs::state_dir()?.to_str().map(|path| path.to_string())
-}
-
-/// Returns the path to the user's audio directory.
-#[php_function]
-pub fn audio_dir() -> Option<String> {
-    dirs::audio_dir()?.to_str().map(|path| path.to_string())
-}
-
-/// Returns the path to the user's desktop directory.
-#[php_function]
-pub fn desktop_dir() -> Option<String> {
-    dirs::desktop_dir()?.to_str().map(|path| path.to_string())
-}
-
-/// Returns the path to the user's document directory.
-#[php_function]
-pub fn document_dir() -> Option<String> {
-    dirs::document_dir()?.to_str().map(|path| path.to_string())
-}
-
-/// Returns the path to the user's download directory.
-#[php_function]
-pub fn download_dir() -> Option<String> {
-    dirs::download_dir()?.to_str().map(|path| path.to_string())
-}
-
-/// Returns the path to the user's font directory.
-#[php_function]
-pub fn font_dir() -> Option<String> {
-    dirs::font_dir()?.to_str().map(|path| path.to_string())
-}
-
-/// Returns the path to the user's picture directory.
-#[php_function]
-pub fn picture_dir() -> Option<String> {
-    dirs::picture_dir()?.to_str().map(|path| path.to_string())
-}
-
-/// Returns the path to the user's public directory.
-#[php_function]
-pub fn public_dir() -> Option<String> {
-    dirs::public_dir()?.to_str().map(|path| path.to_string())
-}
-
-/// Returns the path to the user's template directory.
-#[php_function]
-pub fn template_dir() -> Option<String> {
-    dirs::template_dir()?.to_str().map(|path| path.to_string())
-}
-
-/// Returns the path to the user's video directory.
-#[php_function]
-pub fn video_dir() -> Option<String> {
-    dirs::video_dir()?.to_str().map(|path| path.to_string())
-}
+// Generate all directory functions
+dirs_fn!(home_dir, "Returns the path to the user's home directory.");
+dirs_fn!(cache_dir, "Returns the path to the user's cache directory.");
+dirs_fn!(config_dir, "Returns the path to the user's config directory.");
+dirs_fn!(config_local_dir, "Returns the path to the user's local config directory.");
+dirs_fn!(data_dir, "Returns the path to the user's data directory.");
+dirs_fn!(data_local_dir, "Returns the path to the user's local data directory.");
+dirs_fn!(executable_dir, "Returns the path to the user's executable directory.");
+dirs_fn!(preference_dir, "Returns the path to the user's preference directory.");
+dirs_fn!(runtime_dir, "Returns the path to the user's runtime directory.");
+dirs_fn!(state_dir, "Returns the path to the user's state directory.");
+dirs_fn!(audio_dir, "Returns the path to the user's audio directory.");
+dirs_fn!(desktop_dir, "Returns the path to the user's desktop directory.");
+dirs_fn!(document_dir, "Returns the path to the user's document directory.");
+dirs_fn!(download_dir, "Returns the path to the user's download directory.");
+dirs_fn!(font_dir, "Returns the path to the user's font directory.");
+dirs_fn!(picture_dir, "Returns the path to the user's picture directory.");
+dirs_fn!(public_dir, "Returns the path to the user's public directory.");
+dirs_fn!(template_dir, "Returns the path to the user's template directory.");
+dirs_fn!(video_dir, "Returns the path to the user's video directory.");
 
 #[php_module]
 pub fn get_module(module: ModuleBuilder) -> ModuleBuilder {
